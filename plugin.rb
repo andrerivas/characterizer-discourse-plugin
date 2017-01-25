6c7fcecad83d2bec551d75696c779a3df28bf4cc
Andre Rivas <andre.rivas@gmail.com>
2017-01-25 14:04:07 -0800

    Incrementing version

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: characterizer
 # about: A plugin to auto tag character names in topics. Very basic!
-# version: 0.0.2
+# version: 0.0.3
 # authors: Andre Rivas
 
 enabled_site_setting :characterizer_plugin_enabled
